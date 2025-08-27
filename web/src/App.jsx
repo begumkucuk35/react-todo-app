c287964d2f9f5ac2cd3edd91d1e3df75b0af1551
begum_kucuk9 <begumkucuk9@izmir.bel.tr>
2025-08-27 12:48:36 +0300

    React confetti added for all tasks completed

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import btnAdd from "./assets/btn-add.svg";
 import completeTask from "./assets/check.svg";
 import deleteTask from "./assets/trash.svg";
+import Confetti from "react-confetti";
 import "./App.css";
 
 function App() {
@@ -55,31 +56,42 @@ function App() {
         </button>
       </div>
       <div className="tasks-container">
-        <h4>Tasks to do - {todos.filter(t => !t.completed).length}</h4>
-        <ul>
-          {todos.filter(t => !t.completed).map((task) => (
-            <li key={task.id} className="task-todo">
-              <span>{task.name}</span>
-              <div className="actions">
-                <button onClick={() => toggleComplete(task.id)}>
-                  <img src={completeTask} alt="Complete Task" />
-                </button>
-                <button onClick={() => deleteTodo(task.id)}>
-                  <img src={deleteTask} alt="Delete Task" />
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <h4>Tasks to do - {todos.filter((t) => !t.completed).length}</h4>
+        {todos.filter((t) => !t.completed).length == 0 && todos.length > 0 ? (
+          <div>
+            <p className="congrats-text">Congratulations! You completed all of your tasks...</p>
+          <Confetti/>
+          </div>
+        ) : (
+          <ul>
+            {todos
+              .filter((t) => !t.completed)
+              .map((task) => (
+                <li key={task.id} className="task-todo">
+                  <span>{task.name}</span>
+                  <div className="actions">
+                    <button onClick={() => toggleComplete(task.id)}>
+                      <img src={completeTask} alt="Complete Task" />
+                    </button>
+                    <button onClick={() => deleteTodo(task.id)}>
+                      <img src={deleteTask} alt="Delete Task" />
+                    </button>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        )}
       </div>
       <div className="tasks-container">
-        <h4>Done - {todos.filter(t => t.completed).length}</h4>
+        <h4>Done - {todos.filter((t) => t.completed).length}</h4>
         <ul>
-          {todos.filter(t => t.completed).map((task) => (
-            <li key={task.id} className="task-done">
-              <span>{task.name}</span>
-            </li>
-          ))}
+          {todos
+            .filter((t) => t.completed)
+            .map((task) => (
+              <li key={task.id} className="task-done">
+                <span>{task.name}</span>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
