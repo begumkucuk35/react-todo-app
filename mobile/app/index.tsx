7ae4ed674048f4fc575f19db0f40d855d07a9a96
begum_kucuk9 <begumkucuk9@izmir.bel.tr>
2025-08-27 15:58:20 +0300

    Creating states

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,9 +1,51 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function Index() {
+interface Todo {
+  id: string;
+  name: string;
+  completed: boolean;
+}
+
+export default function index() {
+  const [task, setTask] = useState<string>(""); // tasks in input
+  const [todos, setTodos] = useState<Todo[]>([
+    {
+      id: "1",
+      name: "Study lesson",
+      completed: false,
+    },
+  ]); // todo list
+
+  // Add the task
+  const addTodo = (): void => {};
+
+  // Delete the task
+  const deleteTodo = (id: string): void => {};
+
+  // Complete the task
+  const toggleComplete = (id: string): void => {};
+
   return (
     <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View>
+        <TextInput
+          placeholder="Add a task..."
+          value={task}
+          onChangeText={setTask}
+        />
+        <TouchableOpacity onPress={addTodo}>
+          <Image source={require("../assets/images/btn-add.svg")} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({});
